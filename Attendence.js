d38d4e6e51058a7296bd75e5e7bac44b8d372b6e
geetanjali89 <51170105+geetanjali89@users.noreply.github.com>
2019-11-22 10:52:01 +0530

    new

--- Attendence.js
+++ Attendence.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react' ;
-import Attendleft from './Attendleft';
+import Menumain from './Menumain';
 import Navbar from './Navbar';
-import './Users.css' ;
+import './Leftbar.css';
+import {Switch, Route, Link} from 'react-router-dom' ;
+import Userdetail from './Userdetail';
+
 
 class Attendence extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            id: '',
+            show:false
+        };
+    }
+    handleChange = (e) => {
+        this.setState({
+           id: e.target.id
+        });
+    }    
+    keyPressed = (e) => {
+        if(e.key === "Enter") {
+            this.setState({
+                id: e.target.id,
+                show:true
+            });
+            console.log('validate');
+        }
+    }
+    
     render() {
-       
+        console.log("clicked!!!");
         return(
             <div>
                 <Navbar />
-                <Attendleft />
-                <div class="row entries">
-                    <button className="btn btn-block btn-lg btn-outline-info my-2 my-sm-0" type="submit">Add Attendence</button>
+                <div className="container-fluid p-0"> 
+                <div className="leftlist shadow p-3 mb-0">
+                <div>
+                    <Link to ="/main">
+                        <i class="fas fa-home"></i>
+                    </Link>
+                    <Switch>
+                        <Route path="/main" component={Menumain} />
+                    </Switch> 
                 </div>
+                {/* <form > */}
+                    <div className="row">
+                    <div className="col">
+                        <input type="text" className="form-control bg-light" placeholder="ID"
+                            onChange= {this.handleChange}
+                            onKeyPress={this.keyPressed} 
+                            id={this.state.id} />
+                            {
+                                this.state.show === false ? (null):
+                                (
+                                   <Userdetail history={this.props.history}/>
+                                // <Userdetail />
+                                )
+                            }
+                    <br />
+                    </div>
+                    </div>
+                {/* </form> */}
+                </div>
+            </div>
             </div>
         );
     }
-}
+}       
 
 export default Attendence;
